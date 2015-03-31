4bcd0aa86a45aa4034efa8ff58f4e010cd75c745
David Brown <davidbrownza@outlook.com>
2015-03-31 11:37:37 +0200

    Change DB to sqlite

--- src/JMS/settings.py
+++ src/JMS/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'JMS',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'jms',
