a36ff8586f484fdb606cc0a0aa9bbfd9f117edcb
David Brown <davidbrownza@outlook.com>
2018-06-25 22:23:39 +0200

    Authentication via the new Impersonator service
    This is still a rough POC and will be cleaned up later

--- src/filemanager/backends.py
+++ src/filemanager/backends.py
@@ -5,43 +5,44 @@ from django.conf import settings
 from utilities.security.cryptography import PubPvtKey
 from filemanager.models import FileManagerSettings
 
-import pexpect, base64, requests
+import json, requests
 
 
 class LinuxBackend(ModelBackend):
-    
-    key_file = settings.JMS_SETTINGS["impersonator"]["key"]
-    imp_url = "http://127.0.0.1:%s/impersonate" % settings.JMS_SETTINGS["impersonator"]["port"]
-    
+
+    impersonator_settings = settings.JMS_SETTINGS["impersonator"]
+    impersonator_endpoint = "http://%s:%s/tokens" % (
+        impersonator_settings["host"],
+        impersonator_settings["port"]
+    )
+
     def authenticate(self, username=None, password=None):
-        
-        # get the user if it exists; if it doesn't exist, create the user
         user = None
         try:
             user = User.objects.get(username=username)
         except User.DoesNotExist, ex:
             user = User.objects.create(username=username, email="", password="")
-        
-        # encrypt and encode password for transmission and to store in DB
-        encoded = ""
-        with open(self.key_file, "r") as key_fd:
-            key = key_fd.read()
-            encrypted = PubPvtKey.encrypt(key, str("%s:%s" % (username, password)))
-            encoded = base64.b64encode(encrypted)                
-        
-        # send encoded, encrypted password to impersonator server for authentication   
-        if self.linux_auth(encoded):
+
+        payload = {
+            "username": username,
+            "password": password
+        }
+
+        token = self.linux_auth(payload)
+
+        if token:
             fm_settings, created = FileManagerSettings.objects.get_or_create(User=user)
-            fm_settings.ServerPass = base64.b64encode(encrypted)
-            fm_settings.save()                
-            
+
+            fm_settings.ServerPass = token
+            fm_settings.save()
+
             return user
         else:
             return None
-            
-    
-       
-    def linux_auth(self, encoded):
-        data = "%s\n%s\nprompt" % (encoded, "whoami")
-        r = requests.post(self.imp_url, data=data)
-        return r.status_code == requests.codes.ok
+
+    def linux_auth(self, payload):
+        r = requests.post(self.impersonator_endpoint, json=payload)
+        if r.status_code == requests.codes.ok:
+            return r.text
+        else:
+            return None
